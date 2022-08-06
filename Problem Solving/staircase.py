3d025d978e9f1b69ef1ad68f9dd4ebc29663dcca
Ankur Gajurel <ankurgajurel02@gmail.com>
2022-08-06 22:20:30 +0545

    staircase

---
+++ Problem Solving/staircase.py
@@ -0,0 +1,24 @@
+#https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the staircase function below.
+def staircase(n):
+    for i in range(0,n):
+        for j in range(0,n):
+            if i + j >= n-1:
+                print("#",end='') 
+            else:
+                print(" ",end='')
+        print("\r")
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
